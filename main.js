e2acb65025a42e523c62a68fdfac4c0d135699b3
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-29 16:40:35 -0500

    ejercicio 5

--- main.js
+++ main.js
@@ -3,19 +3,29 @@ addEventListener("DOMContentLoaded", (e) => {
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let men = 20;
-        let primer = 10;
+        let suma = 0;
+        let a = 0;
+        let b = 0;
+        let c = 0;
 
-        //let est = document.getElementById("num1").value;
-        alert('el 1 mes el valor a pagar es:'+primer+ ' pesos');
-        for (i=2; i<=20; i++){
-            primer = primer * 2,
-            alert('el '+i+ ' mes el valor a pagar es de: '+primer+ ' pesos');
+        let ventas = document.getElementById("num1").value;
 
+        for (i=1; i<=ventas; i++){
+            let cP = parseInt(prompt('ingrese el valor de la venta:'));
+            
+            if( cP > 1000){
+                a = a + 1;
+            }
+            else if(1000 >= cP && cP >500){
+                b = b + 1;
+            }
+            else{
+                c = c + 1;
+            }
+            suma += cP;
 		}
+        alert('ventas mayores a 1000: '+a+'\nventas mayores a 500 menores o iguales a 1000:'+b+'\nventas menores o iguales a 500: '+c+'\nmonto total: '+suma);
         
-  
 
-        //document.getElementById('res1').innerHTML = lista;
     })
 })
