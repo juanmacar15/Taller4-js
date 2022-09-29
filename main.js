3fa5f2c660042a56a20640ca044596e7d41a9ecf
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-29 16:04:34 -0500

    ejercicio 2

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ addEventListener("DOMContentLoaded", (e) => {
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let num = document.getElementById("num1").value;
-        if(num > 0){
-            for(let i = 1; i < 11; i++){
-                let mult = num * i;
-                alert(num +' x '+ i + '='+ mult)
-            }
-        }
+        let lista = [];
+        let n1 = document.getElementById("num1").value;
+        for (i=1; i<=n1; i++){
+			if (i%2!=0){
+				n2=i;
+			lista.push(n2+"");
+			}
+			else{
+				n2=i+3;
+				lista.push(n2+"");
+			}
+		}
+        document.getElementById('res1').innerHTML = lista;
     })
 })
