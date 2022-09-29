230915331ae8988958ef58f91341478c90a11709
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-29 18:37:22 -0500

    ejercicio 9

--- main.js
+++ main.js
@@ -5,17 +5,24 @@ addEventListener("DOMContentLoaded", (e) => {
 
 
         let cant = document.getElementById("num1").value;
-
+        let mayor = 50000;
+        const iva1 = 0.19;
         for (i=1; i<=cant; i++){
-            let n1 = parseInt(prompt(i+'-'+cant+'\ningrese 1er numero:'));
-            let n2 = parseInt(prompt('ingrese 2do numero:'));
+            let valorAr = parseInt(prompt(i+'-'+cant+'\ningrese el valor del articulo:'));
+            let unid = parseInt(prompt('unidades compradas:'));
 
-            let suma = n1 + n2;
-            let resta = n1-n2;
-            let mul = n1 * n2;
-            let div = n1 / n2;
-            alert('suma: '+suma+ '\nresta: '+resta+ '\nmultiplicacion: '+mul+'\ndivision:'+div)
-            
+            let subTotal = valorAr * unid;
+            let iva2 = subTotal * iva1;
+            let totalPagar = subTotal + iva2;
+
+            if(totalPagar > mayor){
+                let des = totalPagar * 0.15;
+                let descTotal = totalPagar - des;
+                alert('esta compra supero los 500.000 obten tu descuento: \npago con IVA: '+totalPagar+'\ntotal a pagar: '+descTotal+' GRACIAS POR TU COMPRA <3');
+            }
+            else{
+                alert('subtotal: '+subTotal+'\n IVA: '+iva1+'\ntotal a pagar: '+totalPagar);
+            }       
 		}
     })
 })
